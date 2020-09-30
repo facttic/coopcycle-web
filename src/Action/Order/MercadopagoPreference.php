6423596304a72af1b1a63d27adf6fd80e4835d8f
David Maximiliano Sosa <maxi3390@gmail.com>
2020-09-30 18:50:46 -0300

    Added connection with MP accounts

--- src/Action/Order/MercadopagoPreference.php
+++ src/Action/Order/MercadopagoPreference.php
@@ -6,19 +6,32 @@ namespace AppBundle\Action\Order;
 use AppBundle\Entity\Sylius\Order;
 use AppBundle\Service\OrderManager;
 use Doctrine\Persistence\ManagerRegistry;
+use Sylius\Component\Currency\Context\CurrencyContextInterface;
 use Sylius\Component\Payment\Model\PaymentInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use MercadoPago;
+use AppBundle\Entity\MercadopagoAccount;
+use AppBundle\Service\MercadopagoManager;
 
 class MercadopagoPreference
 {
     private $dataManager;
     private $doctrine;
+    private $mercadopagoManager;
+    private $currencyContext;
 
-    public function __construct(OrderManager $dataManager, ManagerRegistry $doctrine)
+    public function __construct(
+        OrderManager $dataManager,
+        ManagerRegistry $doctrine,
+        MercadopagoManager $mercadopagoManager,
+        CurrencyContextInterface $currencyContext
+    )
     {
         $this->orderManager = $dataManager;
         $this->doctrine = $doctrine;
+        $this->mercadopagoManager = $mercadopagoManager;
+        $this->currencyContext = $currencyContext;
     }
 
     public function __invoke($data, Request $request)
@@ -34,6 +47,35 @@ class MercadopagoPreference
             }
         }
 
-        return $applicationFee;
+        $customer = $data->getCustomer();
+        $orderItems = $data->getItems();
+
+        $this->mercadopagoManager->configure();
+        $preference = new MercadoPago\Preference();
+
+        $mpItems = [];
+        foreach ($orderItems as $i) {
+            $product = $i->getVariant();
+            $item = new MercadoPago\Item();
+            $item->id = $i->getId(); // or it could be $i->getCode()
+            $item->title = $product->getName();
+            // $item->description = $product->getDescriptor();
+            $item->quantity = $i->getQuantity();
+            $item->currency_id = $this->currencyContext->getCurrencyCode();
+            $item->unit_price = $i->getUnitPrice() / 100;
+            $mpItems[] = $item;
+        }
+
+        $payer = new MercadoPago\Payer();
+        $payer->email = $customer->getEmail();
+
+        $preference->items = $mpItems;
+        $preference->payer = $payer;
+        $preference->marketplace_fee = $applicationFee / 100;
+        $preference->notification_url = "http://urlmarketplace.com/notification_ipn";
+
+        $preference->save();
+
+        return $preference->id;
     }
 }
