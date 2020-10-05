a9a08592abeb29835f81a34c9005e4044ee8d529
Ivan Schweikofski <schweikofski@gmail.com>
2020-10-05 18:48:46 -0300

    Added validation to unlink MP account. Added some translations.

--- src/Controller/Utils/RestaurantTrait.php
+++ src/Controller/Utils/RestaurantTrait.php
@@ -1307,53 +1307,65 @@ trait RestaurantTrait
 
         $mercadopagoManager->configure();
 
-        // request to Mercadopago
-        // curl -X DELETE https://api.mercadolibre.com/users/$USER_ID/applications/$APP_ID?access_token=$ACCESS_TOKEN
-        $USER_ID        = $mercadopagoAccount->getUserId();
-        $ACCESS_TOKEN   = $mercadopagoAccount->getAccessToken();
-        $APP_ID         = $settingsManager->get('mercadopago_app_id');
-        $url = "https://api.mercadolibre.com/users/{$USER_ID}/applications/{$APP_ID}?access_token={$ACCESS_TOKEN}";
-        
-        // curl delete
-        $ch = curl_init();
+        $restaurant = $restaurantMercadopagoAccounts->getRestaurant();
+        $orders = $restaurant->getOrders();
 
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-        
-        $curl_result = json_decode(curl_exec($ch), true);
-        $curl_httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        
-        curl_close($ch); // do not forget
+        if ( $orders->isEmpty() ) { # no pending orders
 
-        if ($curl_httpCode !== 200 ) { // error
+            // request to Mercadopago
+            // curl -X DELETE https://api.mercadolibre.com/users/$USER_ID/applications/$APP_ID?access_token=$ACCESS_TOKEN
+            $USER_ID        = $mercadopagoAccount->getUserId();
+            $ACCESS_TOKEN   = $mercadopagoAccount->getAccessToken();
+            $APP_ID         = $settingsManager->get('mercadopago_app_id');
+            $url = "https://api.mercadolibre.com/users/{$USER_ID}/applications/{$APP_ID}?access_token={$ACCESS_TOKEN}";
             
+            // curl delete
+            $ch = curl_init();
+
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+            
+            $curl_result = json_decode(curl_exec($ch), true);
+            $curl_httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            
+            curl_close($ch); // do not forget
+
+            if ($curl_httpCode !== 200 ) { // error
+                
+                $this->addFlash(
+                    'error',
+                    $curl_result['message'] ." ".  $curl_result['error']
+                );
+            
+            } elseif ($curl_httpCode === 200) {
+                
+                // $restaurant = $restaurantMercadopagoAccounts->getRestaurant();
+                $restaurant->setEnabled(false);
+                
+                // remove from DB
+                $em = $this->getDoctrine()->getManager();
+                
+                $em->remove($restaurantMercadopagoAccounts);
+                $em->remove($mercadopagoAccount);
+                $em->flush();
+                
+                $this->addFlash(
+                    'notice',
+                    $this->get('translator')->trans('form.local_business.mercadopago_account.unlink.success')
+                );
+
+            } else {
+                $this->addFlash(
+                    'warning',
+                    $this->get('translator')->trans('form.local_business.mercadopago_account.unlink.error')
+                );
+            }
+
+        } else { # pending orders
             $this->addFlash(
                 'error',
-                $curl_result['message'] ." ".  $curl_result['error']
-            );
-        
-        } elseif ($curl_httpCode === 200) {
-            
-            $restaurant = $restaurantMercadopagoAccounts->getRestaurant();
-            $restaurant->setEnabled(false);
-            
-            // remove from DB
-            $em = $this->getDoctrine()->getManager();
-            
-            $em->remove($restaurantMercadopagoAccounts);
-            $em->remove($mercadopagoAccount);
-            $em->flush();
-            
-            $this->addFlash(
-                'notice',
-                $this->get('translator')->trans('form.local_business.mercadopago_account.unlink.success')
-            );
-
-        } else {
-            $this->addFlash(
-                'warning',
-                $this->get('translator')->trans('form.local_business.mercadopago_account.unlink.error')
+                $this->get('translator')->trans('form.local_business.mercadopago_account.unlink.error_pending_orders')
             );
         }
 
